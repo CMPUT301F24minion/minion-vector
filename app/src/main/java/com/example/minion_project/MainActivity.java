d74c2f18ee0d5e257758c29a3f70769334aee50c
Mosayaqoobi <yaqoobi@ualberta.ca>
2024-10-16 21:59:10 -0600

    added toast

--- app/src/main/java/com/example/minion_project/MainActivity.java
+++ app/src/main/java/com/example/minion_project/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.minion_project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //comment
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.start);
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
     }
 }
