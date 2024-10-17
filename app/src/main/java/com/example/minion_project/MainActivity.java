d531587f144dfbb9832b2d13b538b724018ed9e4
preetgami <preetgami20@gmail.com>
2024-10-17 17:47:48 -0600

    finished login/signup logic

--- app/src/main/java/com/example/minion_project/MainActivity.java
+++ app/src/main/java/com/example/minion_project/MainActivity.java
@@ -1,72 +1,118 @@
 package com.example.minion_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-
+import android.provider.Settings.Secure;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.CollectionReference;
-
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
-
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private CollectionReference usersRef;
+    private String android_id;
 
-    Button login;
+    Button loginBtn,userBtn,organizerBtn,adminBtn;
+    TextView choosePageText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); //comment
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         db = FirebaseFirestore.getInstance();
-        login = findViewById(R.id.start);
+        loginBtn = findViewById(R.id.start);
+        userBtn = findViewById(R.id.userBtn);
+        organizerBtn = findViewById(R.id.organizerBtn);
+        adminBtn=findViewById(R.id.adminBtn);
+        choosePageText=findViewById(R.id.choose_page_launch);
         usersRef = db.collection("Users");
 
-
-        login.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                // Assume you have a valid document ID or you need to specify it
+                // Get device id
+                android_id = Secure.getString(getContentResolver(), Secure.ANDROID_ID);
 
-
-                usersRef.document("1riLKOf2Wh3ialkdZTV8").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                // Check if user exists
+                usersRef.document(android_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         if (task.isSuccessful()) {
                             DocumentSnapshot document = task.getResult();
-                            if (document.exists()) {
-                                // Get your data from the document, e.g., a field called "username"
-                                String username = document.getString("Name");
+                            if (document != null && document.exists()) {
+                                Map<String, Object> data = document.getData();
+                                if (data!=null) {
+                                    // User exists, retrieve data
+                                    // this type checking prevents app to still function if something
+                                    // breaks
+                                    Object roleObj = data.get("Roles");
+                                    if (roleObj instanceof Map) {
+                                        Map<String, Boolean> role = (Map<String, Boolean>) roleObj;
+                                        loginBtn.setVisibility(View.GONE); //hide log in button
+                                        choosePageText.setVisibility(View.VISIBLE);
+                                        // check which roles user can access then allow those buttons
+                                        if (role.get("Admin") != null && role.get("Admin")) {
+                                            adminBtn.setVisibility(View.VISIBLE);
+                                        }
+                                        if (role.get("User") != null && role.get("User")) {
+                                            userBtn.setVisibility(View.VISIBLE);
+                                        }
+                                        if (role.get("Organizer") != null && role.get("Organizer")) {
+                                            organizerBtn.setVisibility(View.VISIBLE);
+                                        }
 
-                                // Show the data in a Toast
-                                Toast.makeText(MainActivity.this, "Username: " + username, Toast.LENGTH_SHORT).show();
+                                    }
+                                    }
                             } else {
-                                Toast.makeText(MainActivity.this, "No such user", Toast.LENGTH_SHORT).show();
+                                // User does not exist, show sign-up fragment
+                                showSignUpFragment();
                             }
                         } else {
-                            Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "Error fetching user data", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
         });
+        userBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, UserActivity.class));
+            }
+        });
+        organizerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(MainActivity.this, OrganizerActivity.class));
+            }
+        });
+        adminBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(MainActivity.this, AdminActivity.class));
+            }
+        });
 
 
+    }
 
-
-
+    private void showSignUpFragment() {
+        loginBtn.setVisibility(View.GONE); //hide log in button
+        SignUpFragment signUpFragment = SignUpFragment.newInstance(usersRef,android_id);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_sign_up, signUpFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/minion_project/SignUpFragment.java
@@ -0,0 +1,109 @@
+package com.example.minion_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpFragment extends Fragment {
+
+    private EditText nameEditText, emailEditText, phoneEditText,cityEditText;
+    private CheckBox organizerCheckBox, userCheckBox;
+    private Button signupButton;
+    private CollectionReference usersRef;
+    private String android_id;
+    // instantiate a fragment and set the arguments passed
+    public static  SignUpFragment newInstance(CollectionReference usersRef,String android_id){
+        SignUpFragment fragment=new SignUpFragment();
+        Bundle args =new Bundle();
+        fragment.setArguments(args);
+        fragment.usersRef=usersRef;
+        fragment.android_id=android_id;
+        return  fragment;
+    };
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        nameEditText = view.findViewById(R.id.name);
+        emailEditText = view.findViewById(R.id.email);
+        phoneEditText = view.findViewById(R.id.phone_number);
+        cityEditText=view.findViewById(R.id.city);
+        signupButton = view.findViewById(R.id.signup_button);
+        organizerCheckBox=view.findViewById(R.id.organizer_checkbox);
+        userCheckBox=view.findViewById(R.id.user_checkbox);
+        signupButton.setOnClickListener(v -> signUpUser());
+
+        return view;
+    }
+
+    private void signUpUser() {
+        String name = nameEditText.getText().toString().trim();
+        String email = emailEditText.getText().toString().trim();
+        String phone = phoneEditText.getText().toString().trim();
+        String city = cityEditText.getText().toString().trim();
+
+        Boolean organizerSeleced=organizerCheckBox.isChecked();
+        Boolean userSeleced=userCheckBox.isChecked();
+
+        if (name.isEmpty()) {
+            Toast.makeText(getActivity(), "Please enter your name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Map<String, Boolean> roles=new HashMap<>();
+        roles.put("Admin",Boolean.FALSE);
+        roles.put("Organizer",organizerSeleced);
+        roles.put("User",userSeleced);
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("Name", name);
+        user.put("Email", email);
+        user.put("Phone_number",phone);
+        user.put("City",city);
+        user.put("Roles",roles);
+
+        usersRef.document(android_id)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("Firestore", "DocumentSnapshot successfully written!");
+                        if (organizerSeleced){
+                            // launch organizer activity
+                             ;
+                        } else if (userSeleced) {
+                            // launch user activity
+                            startActivity(new Intent(getActivity(), UserActivity.class));
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Firestore", "Error writing document", e);
+                    }
+                });
+
+    }
+
+
+}
