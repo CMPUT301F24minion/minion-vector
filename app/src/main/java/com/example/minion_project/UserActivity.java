5f79f154125324325d8718da426584fd560f85ca
gitbubbub <151979008+gitbubbub@users.noreply.github.com>
2024-10-18 17:27:24 -0600

    not working :(

--- app/src/main/java/com/example/minion_project/UserActivity.java
+++ app/src/main/java/com/example/minion_project/UserActivity.java
@@ -1,12 +1,20 @@
 package com.example.minion_project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class UserActivity extends AppCompatActivity {
     // a basic activity
@@ -14,12 +22,16 @@ public class UserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentContainerView2);// Use the correct ID
+        NavController navController = navHostFragment.getNavController();
+
+        // Now you can use navController for navigation actions
+        BottomNavigationView bottomNavigationView = findViewById(R.id.userBottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
+
 }
