e7c631d6bc80762371b0fbe71a472d532050d02c
preetgami <preetgami20@gmail.com>
2024-10-30 17:07:59 -0600

    scan qr!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation("com.google.firebase:firebase-firestore")
     //
+    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- app/src/main/java/com/example/minion_project/user/UserScanFragment.java
+++ app/src/main/java/com/example/minion_project/user/UserScanFragment.java
@@ -1,28 +1,33 @@
 package com.example.minion_project.user;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 
 import com.example.minion_project.R;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserScanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class UserScanFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+    private Button scanQrBtn;
+    private TextView scanQrText;
+
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +35,6 @@ public class UserScanFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment user_scanQR.
-     */
-    // TODO: Rename and change types and number of parameters
     public static UserScanFragment newInstance(String param1, String param2) {
         UserScanFragment fragment = new UserScanFragment();
         Bundle args = new Bundle();
@@ -57,10 +53,41 @@ public class UserScanFragment extends Fragment {
         }
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_scan_q_r, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_scan_q_r, container, false);
+
+        scanQrBtn = view.findViewById(R.id.QrBtn);
+        scanQrText = view.findViewById(R.id.QRvalue);
+
+        scanQrBtn.setOnClickListener(v->{
+            scanQr();
+        });
+        return view;
     }
+
+    private void scanQr(){
+        ScanOptions options=new ScanOptions();
+        options.setCaptureActivity(CaptureAct.class);
+        barLaucher.launch(options);
+    }
+    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result->
+    {
+        if(result.getContents() !=null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i)
+                {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
 }
