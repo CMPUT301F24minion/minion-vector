f66daa535d36da5017a213ecac243d8005747daa
Mosa <124646107+Mosayaqoobi@users.noreply.github.com>
2024-11-22 22:46:23 -0700

    Update SignUpFragment.java

--- app/src/main/java/com/example/minion_project/SignUpFragment.java
+++ app/src/main/java/com/example/minion_project/SignUpFragment.java
@@ -216,7 +216,7 @@ public class SignUpFragment extends Fragment {
             organizer.put("Phone_number", phone);
             ArrayList<String> events = new ArrayList<>();
             organizer.put("Events", events);
-            organizer.put("Facility", null);
+            organizer.put("Facility", false);
             saveDocument(organizersRef, organizer);
         }
         if (userSelected) {
