87047c7868154b4a102aa4fc2a20ab743d2e2b2e
junaidmemon <junaid.memon123@hotmail.com>
2024-11-24 18:42:02 -0700

    UI changes when scanning

---
+++ app/src/androidTest/java/com/example/minion_project/OrganizerActivityTest.java
@@ -0,0 +1,49 @@
+package com.example.minion_project;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.minion_project.organizer.OrganizerActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+
+@RunWith(AndroidJUnit4.class)
+public class OrganizerActivityTest {
+
+    @Rule
+    public ActivityTestRule<OrganizerActivity> activityRule =
+            new ActivityTestRule<>(OrganizerActivity.class);
+
+    @Test
+    public void testCreateEventUI() {
+        // Navigate to the Create Event fragment
+        onView(withId(R.id.menu_organizer_create_event)).perform(click());
+
+        // Check that the Create Event screen is displayed
+        onView(withId(R.id.organizerCreateEvent)).check(matches(isDisplayed()));
+
+        // Enter event title
+        onView(withId(R.id.createEventTitleEditText))
+                .perform(typeText("Test Event"))
+                .check(matches(withText("Test Event")));
+
+        // Enter event details
+        onView(withId(R.id.createEventDetailsEditText))
+                .perform(typeText("Event Details"))
+                .check(matches(withText("Event Details")));
+
+        // Enter number of invitations
+        onView(withId(R.id.createEventInvitationsEditText))
+                .perform(typeText("50"))
+                .check(matches(withText("50")));
+
+
+    }
+}
