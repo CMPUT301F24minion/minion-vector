65feafc417dbf5bcbe35e1367383e372655fd8f3
gitbubbub <151979008+gitbubbub@users.noreply.github.com>
2024-12-02 13:54:59 -0700

    Delete doc/JavaDoc directory

--- doc/JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.minion_project"},{"l":"com.example.minion_project.admin"},{"l":"com.example.minion_project.events"},{"l":"com.example.minion_project.organizer"},{"l":"com.example.minion_project.user"}];updateSearchResults();
